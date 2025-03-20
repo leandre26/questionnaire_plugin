6f475d87e658c4dad973b0c5ba995402d37b14fe
leandre26 <106544409+leandre26@users.noreply.github.com>
2025-03-20 10:25:49 +0100

    Update questionnaire_screen.cpp

--- src/questionnaire_screen.cpp
+++ src/questionnaire_screen.cpp
@@ -25,19 +25,19 @@ Component QuestionnaireScreen::Wrap(std::string name, Component component) {
 }
 
 void QuestionnaireScreen::prepare_questionnaire(ScreenInteractive &screen) {
-  auto input_name = Input(&_name, "Nom");
-  input_name |= CatchEvent([&](Event e) { return reset_field(e, _name); });
-  input_name = Wrap("Nom", input_name);
+  auto input_nom = Input(&_nom, "Nom"); // Changed _name -> _nom
+  input_nom = CatchEvent(input_nom, [&](Event e) { return reset_field(e, _nom); });
+  input_nom = Wrap("Nom", input_nom);
 
   auto input_date = Input(&_date, "JJ/MM/AAAA");
   input_date |= CatchEvent([&](Event e) { return reset_field(e, _date); });
   input_date = Wrap("Date", input_date);
 
-  auto input_pressure = Input(&_pressure, "Pression (bar)");
-  input_pressure |= CatchEvent([&](Event e) {
+  auto input_pression = Input(&_pression, "Pression (bar)"); // Changed _pressure -> _pression
+  input_pression |= CatchEvent([&](Event e) {
     return e.is_character() && !isdigit(e.character()[0]);
   });
-  input_pressure = Wrap("Pression", input_pressure);
+  input_pression = Wrap("Pression", input_pression);
 
   _machine_index = 0;
   auto machine_select = Radiobox(&_machines, &_machine_index);
@@ -46,20 +46,20 @@ void QuestionnaireScreen::prepare_questionnaire(ScreenInteractive &screen) {
   auto b_validate = Button("Valider", screen.ExitLoopClosure());
 
   auto layout = Container::Vertical({
-    input_name,
+    input_nom,
     input_date,
     machine_select,
-    input_pressure,
+    input_pression,
     separator(),
     b_validate
   });
 
   component = Renderer(layout, [=] {
     return vbox({
-      input_name->Render(),
+      input_nom->Render(),
       input_date->Render(),
       machine_select->Render(),
-      input_pressure->Render(),
+      input_pression->Render(),
       separator(),
       b_validate->Render()
     }) | xflex | border;
@@ -73,9 +73,9 @@ void QuestionnaireScreen::load_settings(std::string filename) {
     file >> _settings;
     file.close();
   }
-  _name = _settings.value("name", "");
+  _nom = _settings.value("nom", "");
   _date = _settings.value("date", "");
-  _pressure = _settings.value("pressure", "0");
+  _pression = _settings.value("pression", "0");
 }
 
 void QuestionnaireScreen::save_settings() {
@@ -89,18 +89,15 @@ void QuestionnaireScreen::save_settings() {
 
 void QuestionnaireScreen::update_settings() {
   _settings = {
-      {"name", _name},
+      {"nom", _nom},
       {"date", _date},
-      {"pressure", _pressure},
+      {"pression", _pression},
       {"machine_index", _machine_index}
   };
 }
 
 json QuestionnaireScreen::get_data() {
   update_settings();
-  _data["info"] = _settings;
-  return _data;
-}
   _data["info"] = _settings;
   return _data;
 }
