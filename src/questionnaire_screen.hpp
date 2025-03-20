d472368bd8afb997ce8f99772935fe02bc29c2f3
leandre26 <106544409+leandre26@users.noreply.github.com>
2025-03-20 13:46:24 +0100

    All questions added in questionnaire_screen.hpp

--- src/questionnaire_screen.hpp
+++ src/questionnaire_screen.hpp
@@ -52,12 +52,32 @@ private:
   bool _has_new_output = false;
   int _header_width = 18;
 
+  // Questionnaire fields
   std::string _nom = "";
+  std::string _groupe = "";
   std::string _date = "";
-  int _pression = 0;
-  std::string _pression_str = "0"; 
+  
+  int _zone_index = 0;
+  std::vector<std::string> _zones = {"Initiation S1/S2", "Perfectionnement S3/S4/S5"};
+  
   int _machine_index = 0;
   std::vector<std::string> _machines = {"VX1", "VX2", "VX3", "VX4"};
+
+  std::string _pression_hydrolique_str = "0";
+  std::string _pression_broche_air_str = "0";
+  
+  int _huile_broche_index = 0;
+  int _huile_glissieres_index = 0;
+  int _lubrifiant_index = 0;
+  int _bac_copeaux_index = 0;
+  int _proprete_usinage_index = 0;
+  int _proprete_machine_index = 0;
+  
+  std::string _temperature_bloc_froid_str = "0";
+  std::string _observation = "";
+
+  std::vector<std::string> _niveau_options = {"Haut", "Moyen", "Bas"};
+  std::vector<std::string> _proprete_options = {"Propre", "Moyen", "Non nettoyé"};
 
   json _settings = json::parse("{}");
   json _data;
