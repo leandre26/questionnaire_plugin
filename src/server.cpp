43c80e0d3a24d91ca70f8b0d753bc0bd37bbe584
leandre26 <106544409+leandre26@users.noreply.github.com>
2025-03-24 10:04:59 +0100

    Create server.cpp using crow

---
+++ src/server.cpp
@@ -0,0 +1,14 @@
+#include "crow.h"
+#include "src/questionnaire_screen.hpp"
+
+int main() {
+    crow::SimpleApp app;
+
+    // API route to fetch questionnaire data
+    CROW_ROUTE(app, "/questionnaire")([]() {
+        QuestionnaireScreen questionnaire(30);
+        return questionnaire.get_data().dump();  // Return JSON
+    });
+
+    app.port(8080).multithreaded().run();
+}
